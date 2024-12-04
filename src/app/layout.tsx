d2de2b95834ce380a3238554cd449c19697e6d50
Neelank Swamy <neelcswamy@gmail.com>
2024-12-04 15:26:22 -0500

    [12.4] had to change layout to utilize context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { PokemonProvider } from "@/context/PokemonContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <PokemonProvider>{children}</PokemonProvider>
       </body>
     </html>
   );
