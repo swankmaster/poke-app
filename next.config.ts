df04b31a83cdc3647e5bf28944fbc8b7822bfef3
Neelank Swamy <neelcswamy@gmail.com>
2024-12-04 11:46:29 -0500

    routes may need to be edited during debuggin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination:"/home",
+        permanent: true,
+      },
+      {
+        source: "/pokemon/:id",
+        destination: "/home/pokemon/:id",
+        permanent: false,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
