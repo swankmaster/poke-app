054eba91940699afbda9680e0ea6475cfca0a6d1
Neelank Swamy <neelcswamy@gmail.com>
2025-01-25 12:50:01 -0500

    loading pages created

---
+++ src/app/home/pokemon/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-100">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-t-blue-500 border-gray-300"></div>
+    </div>
+  );
+}
